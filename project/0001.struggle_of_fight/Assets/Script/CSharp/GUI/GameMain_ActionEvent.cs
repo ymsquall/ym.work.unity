bfbf9093b7717239ec50254b26817692a188d4c8
ming.yue <ymsquare_1@163.com>
2014-02-16 22:21:10 +0800

    add gamemainui

---
+++ project/0001.struggle_of_fight/Assets/Script/CSharp/GUI/GameMain_ActionEvent.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameMain_ActionEvent : MonoBehaviour
+{
+	public GameMain_MoveStackPanel mLinkedUI;
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
---
+++ project/0001.struggle_of_fight/Assets/Script/CSharp/GUI/GameMain_MoveStackEvent.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameMain_MoveStackEvent : MonoBehaviour
+{
+	public GameMain_MoveStackPanel mLinkedUI;
+	// Use this for initialization
+	void Start ()
+	{
+		mLinkedUI.OnButtonDown += OnMoveStackButtonDown;
+		mLinkedUI.OnButtonDrag += OnMoveStackButtonDrag;
+		mLinkedUI.OnButtonUp += OnMoveStackButtonUp;
+	}
+	
+	bool OnMoveStackButtonDown(Object sender, System.EventArgs args)
+	{
+		var mouseArgs = args as GameMain_MoveStackPanel.ButtonEventArgs;
+		if (sender == mLinkedUI.mMoveStackBar)
+		{
+			return true;
+		}
+		else if(sender == mLinkedUI.mMoveStackBar)
+		{
+			return true;
+		}
+		return false;
+	}
+	
+	bool OnMoveStackButtonDrag(Object sender, System.EventArgs args)
+	{
+		var mouseArgs = args as GameMain_MoveStackPanel.ButtonEventArgs;
+		if (sender == mLinkedUI.mMoveStackBar)
+		{
+			return true;
+		}
+		else if(sender == mLinkedUI.mMoveStackBar)
+		{
+			return true;
+		}
+		return false;
+	}
+	
+	bool OnMoveStackButtonUp(Object sender, System.EventArgs args)
+	{
+		var mouseArgs = args as GameMain_MoveStackPanel.ButtonEventArgs;
+		if (sender == mLinkedUI.mMoveStackBar)
+		{
+			return true;
+		}
+		else if(sender == mLinkedUI.mMoveStackBar)
+		{
+			return true;
+		}
+		return false;
+	}
+}
