8a5a407f966f91bcc3fb054204e836fabef91df4
ymsquall <ymsquare@hotmail.com>
2014-02-20 18:45:08 +0800

    add horizontal 2d controller

---
+++ project/0001.struggle_of_fight/Assets/Script/CSharp/Controller/Horizontal2DCamera.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using System.Collections;
+
+public class Horizontal2DCamera : MonoBehaviour
+{
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+    }
+}
---
+++ project/0001.struggle_of_fight/Assets/Script/CSharp/Controller/Horizontal2DController.cs
@@ -0,0 +1,200 @@
+using UnityEngine;
+using System.Collections;
+
+[AddComponentMenu("ExternControl/Horizontal2D")]
+public class Horizontal2DController : MonoBehaviour
+{
+    public AnimationClip mAnim02_Jumpup;
+    public AnimationClip mAnim03_Take;
+    public AnimationClip mAnim04_Attack01;
+    public AnimationClip mAnim05_Attack02;
+    public AnimationClip mAnim06_Attack03;
+    public AnimationClip mAnim07_AirAttack;
+    public AnimationClip mAnim08_Run;
+    public AnimationClip mAnim09_BeAttack;
+    public AnimationClip mAnim10_Death;
+    public AnimationClip mAnim11_Take;
+    public AnimationClip mAnim12_JumpAir;
+    public AnimationClip mAnim13_JumpDown;
+    public AnimationClip mAnim14_Idel;
+
+    public float mCharacterGravity = 20.0f;
+    public float mMoveSpeed = 0.0f;
+    public float mSpeedSmoothing = 10.0f;
+    public float mRotateSpeed = 500.0f;
+    public float mTrotAfterSeconds = 3.0f;
+    public float mTrotSpeed = 4.0f;
+    public float mRunSpeed = 6.0f;
+    public float mInAirControlAcceleration = 3.0f;
+    public float mJumpHeight = 0.5f;
+    public float mJumpRepeatTime = 0.05f;
+    public float mJumpTimeout = 0.15f;
+    public float mGroundedTimeout = 0.25f;
+
+    // How high do we jump when pressing jump and letting go immediately
+    public CollisionFlags mCollisionFlags;
+
+    enum CharacterState : byte
+    {
+        Jumpup = 2,
+        Take03 = 3,
+        Attack01 = 4,
+        Attack02 = 5,
+        Attack03 = 6,
+        AirAttack = 7,
+        Running = 8,
+        BeAttack = 9,
+        Death = 10,
+        Take11 = 11,
+        JumpAir = 12,
+        JumpDown = 13,
+        Idel = 14
+    }
+    public static float CalculateJumpVerticalSpeed(float jumpHeight)
+    {
+	    // From the jump height and gravity we deduce the upwards speed 
+	    // for the character to reach at the apex.
+        var thisInstance = GameObject.FindObjectOfType<Horizontal2DController>();
+	    return Mathf.Sqrt(2.0f * jumpHeight * thisInstance.mCharacterGravity);
+    }
+    public bool Grounded
+    {
+        get { return (mCollisionFlags & CollisionFlags.CollidedBelow) != 0; }
+    }
+    public bool Moving
+    {
+        get { return Mathf.Abs(Input.GetAxisRaw("Vertical")) + Mathf.Abs(Input.GetAxisRaw("Horizontal")) > 0.5f; }
+    }
+    public bool Jumping
+    {
+        set { mJumping = value; }
+        get { return mJumping; }
+    }
+    public void UpdateSmoothedMovementDirection()
+    {
+        var cameraTransform = Camera.main.transform;
+
+        // Forward vector relative to the camera along the x-z plane	
+        var forward = cameraTransform.TransformDirection(Vector3.right);
+        forward.y = 0;
+        forward.z = 0;
+        forward = forward.normalized;
+        // Right vector relative to the camera
+        // Always orthogonal to the forward vector
+        var right = new Vector3(0, 0, -forward.x);
+        var h = Input.GetAxisRaw("Horizontal");
+        var wasMoving = mIsMoving;
+        mIsMoving = Mathf.Abs(h) > 0.1;
+        // Target direction relative to the camera
+        var targetDirection = h * right;
+        // Grounded controls
+        if (Grounded)
+        {
+            // Lock camera for short period when transitioning moving & standing still
+            mLockCameraTimer += Time.deltaTime;
+            if (mIsMoving != wasMoving)
+                mLockCameraTimer = 0.0f;
+            // We store speed and direction seperately,
+            // so that when the character stands still we still have a valid forward direction
+            // moveDirection is always normalized, and we only update it if there is user input.
+            if (targetDirection != Vector3.zero)
+            {
+                mMoveDirection = Vector3.RotateTowards(mMoveDirection, targetDirection, mRotateSpeed * Mathf.Deg2Rad * Time.deltaTime, 1000);
+                mMoveDirection = mMoveDirection.normalized;
+            }
+            // Smooth the speed based on the current target direction
+            var curSmooth = mSpeedSmoothing * Time.deltaTime;
+            // Choose target speed
+            //* We want to support analog input but make sure you cant walk faster diagonally than just forward or sideways
+            var targetSpeed = Mathf.Min(targetDirection.magnitude, 1.0f);
+            // Pick speed modifier
+            targetSpeed *= mRunSpeed;
+            mState = CharacterState.Running;
+            mMoveSpeed = Mathf.Lerp(mMoveSpeed, targetSpeed, curSmooth);
+        }
+        else
+        {
+            // In air controls
+            // Lock camera while in air
+            if (mJumping)
+                mLockCameraTimer = 0.0f;
+            if (mIsMoving)
+                mInAirVelocity += targetDirection.normalized * Time.deltaTime * mInAirControlAcceleration;
+        }
+    }
+
+    void Awake()
+    {
+        mMoveDirection = transform.TransformDirection(Vector3.right);
+        mPlayingAnim = GetComponent<Animation>();
+        if (!mPlayingAnim)
+            Debug.Log("The character you would like to control doesn't have animations. Moving her might look weird.");
+        if (!mAnim14_Idel)
+        {
+            mPlayingAnim = null;
+            Debug.Log("No idle animation found. Turning off animations.");
+        }
+    }
+    void Start()
+    {
+    }
+    void ApplyGravity()
+    {
+        // Apply gravity
+        var jumpButton = Input.GetButton("Jump");
+        // When we reach the apex of the jump we send out a message
+        if (mJumping && mVerticalSpeed <= 0.0)
+        {
+            //jumpingReachedApex = true;
+            SendMessage("DidJumpReachApex", SendMessageOptions.DontRequireReceiver);
+        }
+        if (Grounded)
+            mVerticalSpeed = 0.0f;
+        else
+            mVerticalSpeed -= mCharacterGravity * Time.deltaTime;
+    }
+    void ApplyJumping()
+    {
+
+    }
+    void Update()
+    {
+        UpdateSmoothedMovementDirection();
+        // Apply gravity
+        // - extra power jump modifies gravity
+        // - controlledDescent mode modifies gravity
+        ApplyGravity();
+        // Apply jumping logic
+        ApplyJumping();
+        // Calculate actual motion
+        var movement = mMoveDirection * mMoveSpeed + new Vector3(0, mVerticalSpeed, 0) + mInAirVelocity;
+        movement *= Time.deltaTime;
+	    // Move the controller
+	    var controller = GetComponent<CharacterController>();
+        mCollisionFlags = controller.Move(movement);
+
+        if (Grounded)
+        {
+
+        }
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+
+    }
+
+    private Animation mPlayingAnim = null;
+    private CharacterState mState = CharacterState.Idel;
+    private Vector3 mMoveDirection = Vector3.zero;
+    private float mVerticalSpeed = 0.0f;
+    private Vector3 mInAirVelocity = Vector3.zero;
+    private bool mJumping = false;
+    private bool mIsMoving = false;
+    private float mLockCameraTimer = 0.0f;
+}
