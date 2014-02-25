afbc82b30af7382c09999ff189a1651aa49cc553
ymsquall <ymsquare@hotmail.com>
2014-02-25 17:50:55 +0800

    add h2dcc interface

---
+++ project/0001.struggle_of_fight/Assets/Script/CSharp/Controller/Horizontal2D/Character/IH2DCCGravity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Script.CSharp.Controller.Horizontal2D.Character
+{
+    interface IH2DCCGravity : IHorizontal2DCharacterController
+    {
+    }
+}
---
+++ project/0001.struggle_of_fight/Assets/Script/CSharp/Controller/Horizontal2D/Character/IHorizontal2DCharacterController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Script.CSharp.Controller.Horizontal2D.Character
+{
+    interface IHorizontal2DCharacterController
+    {
+
+    }
+}
